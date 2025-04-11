f3fae5a34f5cb51b846efa86989fa7d4ca6dfd9c
Your Name <your.email@example.com>
2025-04-11 18:16:01 +0300

    bcrypt fix

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const db = require("../db/db");
 
 const JWT_SECRET_KEY = "mySuperSecretKey";
--- src/Controllers/tutorController.js
+++ src/Controllers/tutorController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require('../db/db'); // Ensure this path is correct
 
 const registerTutor = async (req, res) => {
